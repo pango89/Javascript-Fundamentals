bf7793cb976e0391d318f06f0ca0e8a4a414bf0a
Pankaj Goyal <pankaj.goyal@optym.com>
2020-04-04 23:11:56 +0530

    Function Borrowing in Javascript

---
+++ Fundamentals/call.borrowing.js
@@ -0,0 +1,18 @@
+let person = {
+  firstname: "Pankaj",
+  lastname: "Goyal",
+
+  printFullname: function () {
+    console.log(this.firstname, " ", this.lastname);
+  },
+};
+
+person.printFullname();
+
+let sportsPerson = {
+  firstname: "Sachin",
+  lastname: "Tendulkar",
+};
+
+// Function Borrowing
+person.printFullname.call(sportsPerson);
